52713d9e7001450c80421615ce7ef79edf745caa
Keerthi-creator-hash <keerthi@stavir.com>
2024-11-28 10:36:06 +0530

    Update start_server.sh

--- Scripts/start_server.sh
+++ Scripts/start_server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/app
-python3 hello.py > /home/ubuntu/hello.log 2>&1 &
+echo "Starting hello.py at $(date)" >> /home/ubuntu/hello.log
+cd /home/ubuntu/myproject || { echo "Failed to change directory"; exit 1; }
+python3 hello.py >> /home/ubuntu/hello.log 2>&1 &
+echo "hello.py started successfully." >> /home/ubuntu/hello.log
